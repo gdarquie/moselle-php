0db52059d56edd986f66000ffa3fe3efca3b9beb
gdarquie <gaetan@shinsen.fr>
2021-03-23 18:36:42 +0100

    Update namespace

--- src/HelloWorld/HelloWorld.php
+++ src/HelloWorld/HelloWorld.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace HelloWorld;
+namespace Moselle\HelloWorld;
 
 class HelloWorld
 {
--- src/Offer/OfferFetcher.php
+++ src/Offer/OfferFetcher.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Moselle;
+namespace Moselle\Offer;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
